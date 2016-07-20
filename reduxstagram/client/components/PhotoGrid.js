e578e59227f6bee0bce3357e8f12b4a7aceb4bed
Sean Rose <sean1rose@gmail.com>
2016-07-19 20:26:24 -0700

    Photo component - added likes and comment boxes and caption

--- reduxstagram/client/components/PhotoGrid.js
+++ reduxstagram/client/components/PhotoGrid.js
@@ -2,7 +2,7 @@
 -rccs -> react stateful component
 -PhotoGrid component receives the array of posts data (receives it as props courtesy of connect)
 	- then want to iterate thru the array and dispaly each one using the Photo Component
-	- Photo component will have various attributes, such as a post attribute, which it will then receive as props??? So it receives the data
+	- Photo component will have various attributes, such as a post attribute, which it will then receive as props so that it receives the data
 
 */
 
